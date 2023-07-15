9c54711c0ba563e4ef9eceb5f407692f8583010c
sahil <gautamsahil>
2023-07-15 23:13:19 +0530

    added keybindings

--- config.def.h
+++ config.def.h
@@ -1,7 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
@@ -11,8 +13,8 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Agave Nerd Font:size=20" };
-static const char dmenufont[]       = "Agave Nerd Font:size=20";
+static const char *fonts[]          = { "Agave Nerd Font:size=15" };
+static const char dmenufont[]       = "Agave Nerd Font:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -83,6 +85,17 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *lightdown[]  = { "light", "-U", "5", NULL };
+static const char *lightup[]  = { "light", "-A", "5", NULL };
+
+static const char *browsercmd[]  = { "brave", NULL };
+static const char *newsboatcmd[]  = { "stnewsboat", NULL };
+
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -141,6 +154,16 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{ MODKEY,                       XK_w,                           spawn,           { .v = browsercmd } },
+	{ MODKEY,                       XK_n,                           spawn,           { .v = newsboatcmd } },
+
+        { 0,                            XF86XK_MonBrightnessUp,         spawn,          {.v = lightup }},
+        { 0,                            XF86XK_MonBrightnessDown,       spawn,          {.v = lightdown } },
+        { 0,                            XF86XK_AudioMute,               spawn,          {.v = mutecmd } },
+        { 0,                            XF86XK_AudioLowerVolume,        spawn,          {.v = voldowncmd } },
+        { 0,                            XF86XK_AudioRaiseVolume,        spawn,          {.v = volupcmd } },
+
 };
 
 /* button definitions */

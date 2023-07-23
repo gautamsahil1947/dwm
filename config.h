48fab5beac778152fd9ab69f966eb083ce24c799
sahil <gautamsahil1947@gmail.com>
2023-07-23 21:01:12 +0530

    clean

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Agave Nerd Font:size=15" };
 static const char dmenufont[]       = "Agave Nerd Font:size=15";
 static const char col_gray1[]       = "#222222";

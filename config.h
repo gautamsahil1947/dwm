4a4dbe71c308e4370984fa41937cde6ca7cac626
sahil <gautamsahil1947@gmail.com>
2023-07-24 05:18:22 +0530

    working before xrdb

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Agave Nerd Font:size=15" };
-static const char dmenufont[]       = "Agave Nerd Font:size=15";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -34,7 +34,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " ", "󰙯 ", " ", "󰻈 ", "󰇮 ", " ", " ", " ", " " };
+static const char *tags[] = { "", "󰙯", "", "󰻈", "󰇮", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -94,7 +94,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *lightdown[]  = { "backlight", "down", NULL };

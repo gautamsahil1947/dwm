0cd8e75c204f442ebcb8dab6db173d8c4bc28ba9
Sahil Gautam <gautamsahil1947@gmail.com>
2023-11-04 00:10:42 +0530

    changed the tags to phie

--- config.h
+++ config.h
@@ -39,8 +39,8 @@ static const unsigned int alphas[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {" ",  " ",  "󰨞 ", " ", "󰭹 ",
-                             "󰟢 ", "󰟢 ", "󰟢 ", "󰟢 "};
+static const char *tags[] = {"󰟢", "󰟢", "󰟢", "󰟢", "󰟢",
+                             "󰟢", "󰟢", "󰟢", "󰟢"};
 
 static const Rule rules[] = {
     /* xprop(1):
